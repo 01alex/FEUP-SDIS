b44243f702b30449f385048c9f9fe042bec4e02f
LuísCosta <luistelmocosta@gmail.com>
2017-05-28 19:58:30 +0100

    respositories

--- GitMobile/src/pages/repos/repos.ts
+++ GitMobile/src/pages/repos/repos.ts
@@ -22,7 +22,10 @@ export class ReposPage {
     })
   }
 
-  goToDetails(name: string) {
+  goToDetails(name: string, login: string) {
+    console.log(name);
+    console.log(login);
     this.navCtrl.push(ReposDetailsPage, {name});
+    this.navCtrl.push(ReposDetailsPage, {login});
   }
 }
