e5e69b037ff4929ec79931b324732c2b238728b3
Guilherme-Routar <groutar@gmail.com>
2017-05-29 02:19:53 +0100

    Profile updated

--- GitMobile/src/pages/profile/profile.ts
+++ GitMobile/src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Authentication } from '../../providers/authentication';
+import { GithubUsers } from '../../providers/github-users/github-users';
 
 @Component({
   selector: 'page-profile',
@@ -9,12 +10,26 @@ import { Authentication } from '../../providers/authentication';
 export class Profile {
   
   username: any;
-  email: any;
-  picture: any;
+  avatar: any;
+  followers: any;
+  following: any;
+  public_repos: any;
+  public_gists: any;
 
-  constructor(public navCtrl: NavController, public auth: Authentication) {
-    this.username = this.auth.getUser().social.github.data.username;
-    this.email = this.auth.getUser().social.github.data.email;
-    this.picture = this.auth.getUser().social.github.data.profile_picture;
+  constructor(public navCtrl: NavController, public auth: Authentication, public user: GithubUsers) {
+
+    user.loadDetails(this.auth.getUser().social.github.data.username).subscribe(
+      result => {
+        this.username = result.login;
+        this.avatar = result.avatar_url;
+        this.followers = result.followers;
+        this.following = result.following;
+        this.public_repos = result.public_repos;
+        this.public_gists = result.public_gists;
+      },
+      () => {
+        console.log('getData completed');
+      }
+    );
   }
 }
